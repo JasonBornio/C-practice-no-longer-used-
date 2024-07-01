50827b651c2e4b9edb5859b6e6180c30f065a9fc
Justin Ugwudike <78433956+JasonBornio@users.noreply.github.com>
2024-07-01 15:30:48 +0100

    added test_register_file.cpp

---
+++ 001_computer_arc/components/cpu/tests/test_register_file.cpp
@@ -0,0 +1,30 @@
+#include "../register_file.h"
+
+int main(){
+
+    Register_File reg_file;
+
+    bool arr[8] = {true, false, true, false, true, false, true, true};
+    bool arr2[8] = {true, true, true, true, true, false, true, false};
+
+    bool arr_data[32];
+
+    Reg_32 reg;
+    reg.fill_arr_lower(arr, 8);
+    reg.get_data_arr(arr_data);
+
+    reg_file.clk_step(1, 2, 1, arr_data);
+
+    reg.fill_arr_lower(arr2, 8);
+    reg.get_data_arr(arr_data);
+    
+    reg_file.clk_step(1, 2, 3, arr_data);
+
+    reg_file.get_rs(arr_data);
+
+    reg.fill_arr_lower(arr_data, 32);
+    reg.set_name("data");
+    reg.print();
+
+    reg_file.print();
+}
