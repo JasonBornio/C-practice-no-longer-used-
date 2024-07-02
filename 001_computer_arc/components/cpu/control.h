b88bcee000b29e4b9a49f12864518e6ef4080719
Justin Ugwudike <78433956+JasonBornio@users.noreply.github.com>
2024-07-02 22:00:53 +0100

    Update control.h
    updated control.h

---
+++ 001_computer_arc/components/cpu/control.h
@@ -0,0 +1,42 @@
+#include "utilities.h"
+
+class Control {
+    public:
+        Control();
+
+        void clock_step(bool inputs[][32], int num_inputs, bool control_signal[2]);
+        void get_output(bool array[32]);
+
+        void print();
+
+    private:
+
+
+
+        bool alu_control[4] = {false, false, false, false};
+
+        bool alu_src;
+        bool reg_dst;
+        bool reg_wrt;
+        bool mem_read;
+        bool mem_wrt;
+        bool mem_to_reg;
+        bool branch;
+
+        bool jump;
+
+
+};
+
+Control::Control(){
+    alu_src = false;
+    reg_dst = false;
+    reg_wrt = false;
+    mem_read = false;
+    mem_wrt = false;
+    mem_to_reg = false;
+    branch = false;
+
+    jump = false;
+}
+
