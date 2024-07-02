516a83b7e9eda1d7f84d11ab5ebd5b4a49074da6
Justin Ugwudike <78433956+JasonBornio@users.noreply.github.com>
2024-07-02 20:19:31 +0100

    Update mux.h
    updated clock_step() and get_output()

--- 001_computer_arc/components/cpu/mux.h
+++ 001_computer_arc/components/cpu/mux.h
@@ -4,14 +4,16 @@ class Multiplexer_2 {
     public:
         Multiplexer_2();
 
-        void clock_step(bool inputs[][32], int num_inputs, bool control_signal[2]);
-        void get_output(bool array[32]);
+        void clock_step(bool inputs[4][32]);
+        void get_output(bool control_signal[2], bool array[32]);
 
         void print();
 
     private:
 
+        bool current_inputs[4][32] = {};
         bool current_output[32] = {};
+        int num_inputs = 0;
 
 
 };
@@ -20,21 +22,22 @@ Multiplexer_2::Multiplexer_2(){
 
 }
 
-void Multiplexer_2::clock_step(bool inputs[][32], int num_inputs, bool control_signal[2]){
+void Multiplexer_2::clock_step(bool inputs[4][32]){
+    for(int i = 0; i < 32; i++){
+        current_inputs[0][i] = inputs[0][i];
+        current_inputs[1][i] = inputs[1][i];
+        current_inputs[2][i] = inputs[2][i];
+        current_inputs[3][i] = inputs[3][i];
+    }
+}
+
+void Multiplexer_2::get_output(bool control_signal[2], bool array[32]){
 
     int index = bin_to_int(control_signal, 2);
 
-    if (index > num_inputs-1) index = num_inputs-1;
-
     for(int i = 0; i < 32; i++){
-        current_output[i] = inputs[index][i];
-    }
-
-}
-
-void Multiplexer_2::get_output(bool array[32]){
-    for(int i = 0; i < 32; i++){
-        array[i] = current_output[i];
+        array[i] = current_inputs[index][i];
+        current_output[i] = current_inputs[index][i];
     }
 }
 
