a080d6dfb73af3f1a6e9a71dccf191b3814400f9
Justin Ugwudike <78433956+JasonBornio@users.noreply.github.com>
2024-07-05 12:32:35 +0100

    Update programcounter.h
    updated function arguments

--- 001_computer_arc/components/cpu/programcounter.h
+++ 001_computer_arc/components/cpu/programcounter.h
@@ -1,51 +1,80 @@
 #include <iostream>
 #include <iomanip>
+#include "control.h"
 
-class Programcounter {
+class Program_Counter {
     public:
-        Programcounter();
+        Program_Counter();
 
         void increment();
-        void branch(int offset);
-        void jump(int address);
-        int get_pc();
+        void branch(bool offset[32]);
+        void jump(bool address[32]);
+        void get_pc(bool arr[32]);
 
         void print();
  
     private:
 
         int text_pointers[2] = { 64, 508 };
-        int pc;
+        Reg_32 pc;
+        Adder_32 adder;
+        
 
 };
 
-Programcounter::Programcounter(){
-    pc = text_pointers[0];
+Program_Counter::Program_Counter(){
+    bool arr[32];
+    int_to_bin(text_pointers[0], arr, 32);
+    pc.fill_arr_lower(arr, 32);
 }
 
-void Programcounter::increment(){
-    pc += 4;
-    if(pc > text_pointers[1]) pc = text_pointers[1];
+void Program_Counter::increment(){
+    pc = adder.add_int(pc, 4);
+    if(pc.get_data() > text_pointers[1]){
+        bool arr[32];
+        int_to_bin(text_pointers[1], arr, 32);
+        pc.fill_arr_lower(arr, 32);
+    }
 }
 
-void Programcounter::branch(int offset){
-    pc += offset * 4;
-    if(pc > text_pointers[1]) pc = text_pointers[1];
-    if(pc < text_pointers[0]) pc = text_pointers[0];
+void Program_Counter::branch(bool offset[32]){
+
+    pc = adder.add_int(pc, 4 + (bin_to_int(offset, 32) * 4));
+
+    if(pc.get_data() > text_pointers[1]){
+        bool arr[32];
+        int_to_bin(text_pointers[1], arr, 32);
+        pc.fill_arr_lower(arr, 32);
+    }
+    if(pc.get_data() < text_pointers[0]){
+        bool arr[32];
+        int_to_bin(text_pointers[0], arr, 32);
+        pc.fill_arr_lower(arr, 32);
+    }
 
 }
 
-void Programcounter::jump(int address){
-    pc = address;
+void Program_Counter::jump(bool address[32]){
+    pc.fill_arr_lower(address, 32);
 
-    if(pc > text_pointers[1]) pc = text_pointers[1];
-    if(pc < text_pointers[0]) pc = text_pointers[0];
+    if(pc.get_data() > text_pointers[1]){
+        bool arr[32];
+        int_to_bin(text_pointers[1], arr, 32);
+        pc.fill_arr_lower(arr, 32);
+    }
+    if(pc.get_data() < text_pointers[0]){
+        bool arr[32];
+        int_to_bin(text_pointers[0], arr, 32);
+        pc.fill_arr_lower(arr, 32);
+    }
 }
 
-int Programcounter::get_pc(){
-    return pc;
+void Program_Counter::get_pc(bool arr[32]){
+    for(int i = 0; i < 32; i++){
+        arr[i] = pc.get_data(i);
+    }
 }
 
-void Programcounter::print(){
-    std::cout << "      PC: " << "0x" << std::setfill('0') << std::setw(3) << std::hex << pc << ", " << std::dec << pc << std::endl; 
+void Program_Counter::print(){
+    std::cout << "      PC: " << "0x" << std::setfill('0') << std::setw(3) << std::hex << pc.get_data() << ", " << std::dec << pc.get_data() << std::endl; 
 }
