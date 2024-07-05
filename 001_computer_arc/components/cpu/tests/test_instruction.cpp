c2ac29358c6f0130500d01820251452696631df4
Justin Ugwudike <78433956+JasonBornio@users.noreply.github.com>
2024-07-05 13:45:41 +0100

    Update test_instruction.cpp
    added more function tests

--- 001_computer_arc/components/cpu/tests/test_instruction.cpp
+++ 001_computer_arc/components/cpu/tests/test_instruction.cpp
@@ -2,6 +2,8 @@
 
 int main(){
 
+    bool arr[32];
+
     r_type_inst inst1 = create_r_type_inst(35,25,8,9,0,4);
     print_r_type_inst(inst1);
 
@@ -9,5 +11,17 @@ int main(){
     print_immediate_inst(inst2);
 
     jump_inst inst3 = create_jump_inst(30, 4350);
+    print_jump_inst(inst3);
+
+    int_to_bin(2000000000, arr, 32);
+    inst1 = create_r_type_inst(arr);
+    print_r_type_inst(inst1);
+
+    int_to_bin(500000000, arr, 32);
+    inst2 = create_immediate_inst(arr);
+    print_immediate_inst(inst2);
+    
+    int_to_bin(1879000000, arr, 32);
+    inst3 = create_jump_inst(arr);
     print_jump_inst(inst3);
 } 
