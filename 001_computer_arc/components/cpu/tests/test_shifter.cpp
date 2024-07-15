fc2aa2ce261c87f2ddc9bd74d1858ec3c6ff87fe
Justin Ugwudike <78433956+JasonBornio@users.noreply.github.com>
2024-07-15 12:28:19 +0100

    Update test_shifter.cpp
    added shifter control

--- 001_computer_arc/components/cpu/tests/test_shifter.cpp
+++ 001_computer_arc/components/cpu/tests/test_shifter.cpp
@@ -4,9 +4,9 @@ int main(){
     Shifter shift_left;
 
     bool arr[32];
-    bool shamt[5];
+    bool shamt[32];
     int_to_bin(65535, arr, 32);
-    int_to_bin(8, shamt, 5);
+    int_to_bin(8, shamt, 32);
 
     Reg_32 result;
     result.set_name("$result");
@@ -14,7 +14,7 @@ int main(){
     shift_left.shift(arr);
     shift_left.print();
 
-    shift_left.clock_step(arr, shamt);
+    shift_left.clock_step(arr, shamt, 0);
     shift_left.get_output(arr);
     
     result.fill_arr_lower(arr, 32);
